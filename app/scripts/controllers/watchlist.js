3d5c954caef125c451cb449cbda6ce2bda084e56
veratech <jared@veratech.co.nz>
2015-08-11 20:36:32 +1200

    110815

--- app/scripts/controllers/watchlist.js
+++ app/scripts/controllers/watchlist.js
@@ -8,10 +8,31 @@
  * Controller of the stockDogApp
  */
 angular.module('stockDogApp')
-  .controller('WatchlistCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('WatchlistCtrl', function ($scope, $routeParams, $modal, WatchlistService, CompanyService) {
+  	//Initializations
+  	$scope.companies = CompanyService.query();
+  	$scope.watchlist = WatchlistService.query($routeParams.listParams.listId);
+  	$scope.stocks = $scope.watchlist.stocks;
+  	$scope.newStock = {};
+  	var addStockModal = $modal({
+  		scope: $scope,
+  		template: 'views/templates/addstock-modal.html',
+  		show: false
+  	});
+
+  	// 2. Expose showStockModal to view via scope
+  	$scope.showStockModal = function() {
+  		addStockModal.$promise.then(addStockModal.show);
+  	};
+
+  	// 3. Call the watchlistModel addStock() function and hide the modal.
+  	$scope.addStock = function (){
+  		$scope.watchlist.addStock({
+  			listId: $routeParams.listId,
+  			company: $scope.newStock.company,
+  			shares: $scope.newStock.shares
+  		});
+  		addStockModal.hide();
+  		$scope.newStock = {};
+  	};
   });
--- app/scripts/services/watchlist-service.js
+++ app/scripts/services/watchlist-service.js
@@ -11,6 +11,48 @@ angular.module('stockDogApp')
   .service('WatchlistService', function WatchlistService() {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
+    //Augment stocks with additional helper functions
+    var StockModel = {
+      save: function () {
+         var watchlist = findById(this.listId);
+         watchlist.recalculate();
+         saveModel();
+      }
+    };
+
+    //Augment wathclists with additional helper functions 
+    var WatchlistModel = {
+      addStock: function (stock) {
+         var existingStock(stock) = _.find(this.stocks, function (s) {
+            return s.company.symbol === stock.company.symbol;
+         });
+
+         if (existingStock) {
+            existingStock.shares += stock.shares;
+         } else {
+            _.extend(stock, StockModel);
+            this.stocks.push(stock);
+         }
+         this.recalculate();
+         saveModel();
+         },
+         removeStock: function (stock){
+            _.remove(this.stocks, function(s) {
+               return s.company.symbol === stock.company.symbol;
+            });
+         this.recalculate();
+         saveModel();
+         },
+         recalculate: function(){
+            var calcs = _.reduce(this.stocks, function(calcs, stock) {
+               calcs.shares += stock.shares;
+               calcs.marketValue += stock.marketValue;
+               calcs.dayChange += stock.dayChange;
+               return calcs;
+            }, { shares: 0, marketValue: 0, dayChange: 0 });
+         }
+      };
+
     //[1] Helper load watchlists from localStorage
    	var loadModel = function (){
    		var model = {
@@ -19,6 +61,12 @@ angular.module('stockDogApp')
    			nextId: localStorage['StockDog.nextId'] ? 
    			parseInt(localStorage['StockDog.nextId']) : 0
    		};
+         _.each(model.watchlists, function (watchlist) {
+            _.extend(watchlist, WatchlistModel);
+            _.each(watchlist.stocks, function (stock){
+               _.extend(stock, StockModel);
+            });
+         });
    		return model;
    	};
 
@@ -45,8 +93,10 @@ angular.module('stockDogApp')
    	};
 
    	//[5] Save a new watchlist to watchlists model
-   	this.save = function (watchlist){
+   	this.save = function (watchlist) {
    		watchlist.id = Model.nextId++;
+         watchlist.stocks = [];
+         _.extend(watchlist, WatchlistModel);
    		Model.watchlists.push(watchlist);
    		saveModel();
    	};
